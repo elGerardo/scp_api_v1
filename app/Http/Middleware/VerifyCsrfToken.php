f25e0c554cb7d5662410ea47e30a4b31939dd355
elGerardo <gerardoyu54@gmail.com>
2024-03-15 21:34:58 -0700

    Except some route to CSFR added

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,13 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        "api/login",
+        "api/v1/scp",
+        "api/v1/scp/{scp_id}",
+        "api/v1/interviews",
+        "api/v1/interviews/{id}",
+        "api/v1/category",
+        "api/v1/category/{name}",
+        "api/v1/enemies/{scp_id}/enemy/{scp_enemy_id}",
     ];
 }
